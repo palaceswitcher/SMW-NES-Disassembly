25bb3c26e5e9935690f6f4cd9c85f187b0a60f70
Ceres Minnie <conmdelta@gmail.com>
2024-12-26 02:02:20 -0500

    Fix Windows diff script

--- diff.bat
+++ diff.bat
@@ -1,2 +1,6 @@
-asm6 smw.asm smw.nes
-(fc /b SuperMarioWorld.nes smw.nes)>"diff.log"
+@echo off
+setlocal
+asm6 smw.asm smw.nes || exit
+fc /b SuperMarioWorld.nes smw.nes
+set res=%errorlevel%
+if "%res%"=="1" (fc /b SuperMarioWorld.nes smw.nes > diff.log)
