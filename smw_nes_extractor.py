e31c6948b3d51ba4c1aa5f6cc2a5e53a5f09a329
PalaceSwitcher <conmdelta@gmail.com>
2025-05-27 21:26:28 -0400

    Dump compressed screens from script

--- smw_nes_extractor.py
+++ smw_nes_extractor.py
@@ -1,4 +1,5 @@
-#SMW NES Data Extractor v1.0.2 by PalaceSwitcher
+#!/usr/bin/python
+# SMW NES Data Extractor v1.1.0 by PalaceSwitcher
 import os
 import sys
 
@@ -19,45 +20,66 @@ while True:
 
 rom_data = f.read() #Read file
 
-# Create directories if they don't exist
-if not os.path.exists("levels"):
-	os.makedirs("levels")
-if not os.path.exists("tilesets"):
-	os.makedirs("tilesets")
+# Export part of rom
+def dump_chunk(data, path, start_offs, end_offs):
+	try:
+		with open(path, "wb") as file:
+			file.write(data[start_offs:end_offs])
+	except PermissionError:
+		print("Error: Permission denied. Unable to write to file \""+path+"\".")
+	except IOError:
+		print("Error: An error occured while writing to file \""+path+"\".")
 
-open("SMW.chr", "wb").write(rom_data[0x80010:0xC0010]) #Dump CHR
+# Create directory if it doesn't exist
+def create_dir(dir):
+	if not os.path.exists(dir):
+		os.makedirs(dir)
+
+# Create directories if they don't exist
+create_dir("levels")
+create_dir("tilesets")
+create_dir("screens")
+
+dump_chunk(rom_data, "SMW.chr", 0x80010, 0xC0010) # Dump CHR
 
 # Dump levels
-open("levels/lvl_1-1.bin", "wb").write(rom_data[0x10:0x2010])
-open("levels/lvl_1-2.bin", "wb").write(rom_data[0x2010:0x4010])
-open("levels/lvl_1-3.bin", "wb").write(rom_data[0x4010:0x6010])
-open("levels/lvl_1-4_1.bin", "wb").write(rom_data[0x6010:0x6910]) #1st part of 1-4
-open("levels/lvl_GhostHouseIntro.bin", "wb").write(rom_data[0x6910:0x6C10]) #Unused screen at end of this
-open("levels/lvl_CastleIntro.bin", "wb").write(rom_data[0x6C10:0x6D10])
-open("levels/lvl_YoshiHouse.bin", "wb").write(rom_data[0x6D10:0x7210]) #0x300 bytes of 0 after this
-open("levels/lvl_1-4_2.bin", "wb").write(rom_data[0x7210:0x7810]) #2nd part of 1-4
-open("levels/lvl_Bonus.bin", "wb").write(rom_data[0x7810:0x8010])
+dump_chunk(rom_data, "levels/lvl_1-1.bin", 0x10, 0x2010)
+dump_chunk(rom_data, "levels/lvl_1-2.bin", 0x2010, 0x4010)
+dump_chunk(rom_data, "levels/lvl_1-3.bin", 0x4010, 0x6010)
+dump_chunk(rom_data, "levels/lvl_1-4_1.bin", 0x6010, 0x6910) # 1st part of 1-4
+dump_chunk(rom_data, "levels/lvl_GhostHouseIntro.bin", 0x6910, 0x6C10) # Unused screen at end of this
+dump_chunk(rom_data, "levels/lvl_CastleIntro.bin", 0x6C10, 0x6D10)
+dump_chunk(rom_data, "levels/lvl_YoshiHouse.bin", 0x6D10, 0x7210) # 0x300 bytes of 0 after this
+dump_chunk(rom_data, "levels/lvl_1-4_2.bin", 0x7210, 0x7810) # 2nd part of 1-4
+dump_chunk(rom_data, "levels/lvl_Bonus.bin", 0x7810, 0x8010)
 
 for i in range(4, 27):
-    open(f"levels/lvl_{i//4+1}-{i%4+1}.bin", "wb").write(rom_data[0x2000*i+0x10:0x2000*i+0x2010])
-open("levels/lvl_7-4.bin", "wb").write(rom_data[0x36010:0x37710])
-open("levels/lvl_BowserRoom.bin", "wb").write(rom_data[0x37710:0x37810])
-open("levels/lvl_ClownCar.bin", "wb").write(rom_data[0x37810:0x38010])
-open("levels/lvl_Title.bin", "wb").write(rom_data[0x5A010:0x5AE10])
-open("levels/lvl_Map.bin", "wb").write(rom_data[0x5C010:0x5D910])
+	dump_chunk(rom_data, f"levels/lvl_{i//4+1}-{i%4+1}.bin", 0x2000*i+0x10, 0x2000*i+0x2010)
+dump_chunk(rom_data, "levels/lvl_7-4.bin", 0x36010, 0x37710)
+dump_chunk(rom_data, "levels/lvl_BowserRoom.bin", 0x37710, 0x37810)
+dump_chunk(rom_data, "levels/lvl_ClownCar.bin", 0x37810, 0x38010)
+dump_chunk(rom_data, "levels/lvl_Title.bin", 0x5A010, 0x5AE10)
+dump_chunk(rom_data, "levels/lvl_Map.bin", 0x5C010, 0x5D910)
 
 # Dump tilesets
 for i in range(28):
-    open(f"tilesets/ts_{i//4+1}-{i%4+1}.bin", "wb").write(rom_data[0x800*i+0x38010:0x800*i+0x38810])
-open("tilesets/ts_YoshiHouse.bin", "wb").write(rom_data[0x46010:0x46810])
-open("tilesets/ts_Unused1.bin", "wb").write(rom_data[0x46810:0x47010])
-open("tilesets/ts_GhostHouseIntro.bin", "wb").write(rom_data[0x47010:0x47810])
-open("tilesets/ts_CastleIntro.bin", "wb").write(rom_data[0x47810:0x48010])
-open("tilesets/ts_Bowser.bin", "wb").write(rom_data[0x4D810:0x4DE10])
-open("tilesets/ts_Bonus.bin", "wb").write(rom_data[0x56010:0x56610])
-open("tilesets/ts_Title.bin", "wb").write(rom_data[0x58010:0x58810])
-open("tilesets/ts_Map.bin", "wb").write(rom_data[0x58810:0x59010])
-open("tilesets/ts_Unused2.bin", "wb").write(rom_data[0x59010:0x59810])
-open("tilesets/ts_Unused3.bin", "wb").write(rom_data[0x59810:0x5A010])
+	dump_chunk(rom_data, f"tilesets/ts_{i//4+1}-{i%4+1}.bin", 0x800*i+0x38010, 0x800*i+0x38810)
+dump_chunk(rom_data, "tilesets/ts_YoshiHouse.bin", 0x46010, 0x46810)
+dump_chunk(rom_data, "tilesets/ts_Unused1.bin", 0x46810, 0x47010)
+dump_chunk(rom_data, "tilesets/ts_GhostHouseIntro.bin", 0x47010, 0x47810)
+dump_chunk(rom_data, "tilesets/ts_CastleIntro.bin", 0x47810, 0x48010)
+dump_chunk(rom_data, "tilesets/ts_Bowser.bin", 0x4D810, 0x4DE10)
+dump_chunk(rom_data, "tilesets/ts_Bonus.bin", 0x56010, 0x56610)
+dump_chunk(rom_data, "tilesets/ts_Title.bin", 0x58010, 0x58810)
+dump_chunk(rom_data, "tilesets/ts_Map.bin", 0x58810, 0x59010)
+dump_chunk(rom_data, "tilesets/ts_Unused2.bin", 0x59010, 0x59810)
+dump_chunk(rom_data, "tilesets/ts_Unused3.bin", 0x59810, 0x5A010)
+
+# Dump screens
+dump_chunk(rom_data, "screens/TitleLogo.bin", 0x5401A, 0x54123)
+dump_chunk(rom_data, "screens/EndingScreen.bin", 0x54123, 0x541F2)
+dump_chunk(rom_data, "screens/ThankYouScreen.bin", 0x541F2, 0x5448C)
+dump_chunk(rom_data, "screens/CopyrightScreen.bin", 0x5448C, 0x544E2)
+dump_chunk(rom_data, "screens/YoshiHouse.bin", 0x544E2, 0x5473B)
 
 print("Data dumped successfully.")
